c273aac9a98c293167e6f069cb198ede667029ae
phamngocan97 <phamngocan97123@gmail.com>
2017-04-20 23:09:42 +0700

    Update Infor.cpp

--- Infor.cpp
+++ Infor.cpp
@@ -6,10 +6,14 @@ Infor::Infor()
 {
 }
 
-Infor::Infor(int SoMonHoc, int SoLop){
+Infor::Infor(int SoMonHoc, int SoLop) {
 	monHoc = new MonHoc[SoMonHoc];
-	lop = new Lop[SoLop];
-	for(int i=0;i<SoLop;i++) lop[i].soSv=0;
+	lop = new Lop*[SoLop];
+	for (int i = 0; i < SoLop; i++) {
+		lop[i] = new Lop();
+		lop[i]->soSv = 0;
+	}
+	sv = new SinhVien();
 	_PmonHoc = 0;
 	_Plop = 0;
 	_SoMonHoc = SoMonHoc;
@@ -22,88 +26,57 @@ Infor::~Infor()
 	delete[]lop;
 }
 
-int Infor::TestLop(int malop){
+int Infor::TestLop(string malop) {
 	for (int i = 0; i < _Plop; i++) {
-		if (malop == lop[i].maLop) return i;
+		if (malop == lop[i]->maLop) return i;
 	}
 
 	return -1;
 }
 
-int Infor::TestLop(string tenlop) {
-	for (int i = 0; i < _Plop; i++) {
-		if (tenlop == lop[i].tenLop) return i;
-	}
 
-	return -1;
-}
-
-int Infor::AddSv(SinhVien *sv,Lop *_lop){
+int Infor::AddSv(SinhVien *sv, Lop *_lop) {
 	int flag;//vitri lop trong mang
-	flag=TestLop(_lop->maLop);
-	if(flag!=-1){//lop da co
-		bool test=TestSV(lop[flag],sv.maSV);
-		if(test){
-			DSSV *temp=lop[flag].dssv;
-			if(lop[flag].soSv==0){
-				 DSSV *ds=new DSSV();
-				ds->sv=sv;
-				ds->next=NULL;
-				temp=ds;
-				 lop[flag].soSv++;
-				 return 1;
+	flag = TestLop(_lop->maLop);
+	if (flag != -1) {//lop da co
+		bool test = TestSV(this->lop[flag], sv->maSV);
+		if (test) {//chua co sv nay
+			DSSV *temp = this->lop[flag]->dssv;
+			while (temp->sv->maSV != sv->maSV && temp->next != NULL) temp = temp->next;
+			if (temp->sv->maSV != sv->maSV) {
+				SinhVien *svTemp = new SinhVien(sv);
+				temp->next = new DSSV();
+				temp->next->sv = svTemp;
+				temp->next->next = NULL;
+				this->lop[flag]->soSv++;
+				return 1;
 			}
-			else{
-			while(temp->sv->maSV!=sv->maSV&&temp->next!=NULL) temp=temp->next;
-				if(temp->sv->maSV!=sv.mssv){
-					DSSV *ds=new DSSV();
-					ds->sv=sv;
-					ds->next=NULL;
-					temp->next=ds;
-					lop[flag].soSv++;
-					return 1;
-				}
-				else{
-					return -1;
-				}
+			else {
+				return -1;
 			}
 		}
 	}
-	else{
-		lop[_Plop]->dssv=new DSSV();
-		lop[_Plop]->maLop=_lop->maLop;
-		lop[_Plop]->tenLop=_lop->tenLop;
-		
-		DSSV *temp=lop[_Plop]->dssv;
-		DSSV *ds=new DSSV();
-		ds->sv=sv;
-		ds->next=NULL;
-		temp=ds;
-		lop[_Plop].soSv++;
-		
+	else {
+		lop[_Plop]->dssv = new DSSV();
+		lop[_Plop]->maLop = _lop->maLop;
+		lop[_Plop]->tenLop = _lop->tenLop;
+
+		DSSV *temp = lop[_Plop]->dssv;
+		SinhVien *svT = new SinhVien(sv);
+		temp->sv = svT;
+		temp->next = NULL;
+		lop[_Plop]->soSv++;
+
 		_Plop++;
 		return 1;
 	}
 }
-bool Infor::TestSV(Lop lop,int mssv) {
-	DSSV *temp = lop.dssv;
+bool Infor::TestSV(Lop *lop, string mssv) {
+	DSSV *temp = lop->dssv;
 	while (temp != NULL) {
 		if (temp->sv->maSV == mssv) return false;
+		temp = temp->next;
 	}
 	return true;
 }
-SinhVien *SinhVien::Init() {
-	SinhVien *temp = new SinhVien();
-	string t;
-	cout << "Nhap Ma Sinh Vien:  ";
-	cin >> temp->maSV;
-	cin.ignore(1);
-	cout << "Nhap Ho: ";
-	getline(cin, t);
-	temp->Ho = t;
-	cout << "Nhap Ten: ";
-	getline(cin, t);
-	temp->Ten = t;
 
-	return temp;
-}
